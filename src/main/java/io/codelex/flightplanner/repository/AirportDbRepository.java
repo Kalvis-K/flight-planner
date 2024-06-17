82979c4a6be063a1159be06c80ccd2077458fe06
Kalvis <kalvis.kaulins@inbox.lv>
2024-06-17 12:23:02 +0300

    Revert

--- src/main/java/io/codelex/flightplanner/repository/AirportDbRepository.java
+++
@@ -1,9 +0,0 @@
-package io.codelex.flightplanner.repository;
-
-import io.codelex.flightplanner.model.Airport;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AirportDbRepository extends JpaRepository<Airport, Long> {
-
-
-}
--- src/main/java/io/codelex/flightplanner/service/AirportService.java
+++ src/main/java/io/codelex/flightplanner/service/AirportService.java
@@ -1,11 +1,32 @@
 package io.codelex.flightplanner.service;
 
 import io.codelex.flightplanner.model.Airport;
+import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public interface AirportService {
-    public void addAirports(List<Airport> airportsToAdd);
-    public List<Airport> searchAirports(@RequestParam String search);
+@Service
+public class AirportService {
+
+    private final List<Airport> airports = new ArrayList<>();
+
+    public void addAirports(List<Airport> airportsToAdd) {
+        airports.addAll(airportsToAdd);
+    }
+
+    public List<Airport> searchAirports(@RequestParam String search) {
+        List<Airport> matchingAirports = new ArrayList<>();
+        String lowercaseSearch = search.toLowerCase().trim();
+        for (Airport airport : airports) {
+
+            if (airport.getAirport().toLowerCase().contains(lowercaseSearch) ||
+                    airport.getCity().toLowerCase().contains(lowercaseSearch) ||
+                    airport.getCountry().toLowerCase().contains(lowercaseSearch)) {
+                matchingAirports.add(airport);
+            }
+        }
+        return matchingAirports;
+    }
 }
